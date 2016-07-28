b904f07cefa316d13499dab9d9bf45ebc06ad205
Delfo <delfo@tech-center.com>
2016-07-28 15:53:48 +0200

    No Data Binding

---
+++ WpfAppSimpleType/01-Win-No-Binding.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppSimpleType
+{
+    /// <summary>
+    /// Interaction logic for _01_Win_No_Binding.xaml
+    /// </summary>
+    public partial class WinNoBinding : Window
+    {
+        public WinNoBinding()
+        {
+            InitializeComponent();
+        }
+
+        private void txtName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblName.Content = txtName.Text;
+            ChangeButtonState();
+        }
+
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            lboElements.Items.Add(txtName.Text);
+        }
+
+        private void ChangeButtonState()
+        {
+            btnAdd.IsEnabled = !string.IsNullOrEmpty(txtName.Text);
+            btnRemove.IsEnabled = !string.IsNullOrEmpty(txtName.Text);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChangeButtonState();
+        }
+
+        private void BtnRemove_OnClick(object sender, RoutedEventArgs e)
+        {
+            int pos = lboElements.Items.IndexOf(txtName.Text);
+            if (pos >= 0)
+                lboElements.Items.RemoveAt(pos);
+            else
+                MessageBox.Show(txtName.Text + " not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ WpfAppSimpleType/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppSimpleType
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnNoBinding_Click(object sender, RoutedEventArgs e)
+        {
+            WinNoBinding win = new WinNoBinding();
+
+            win.Show();
+        }
+
+
+        //private void btnBasicBinding_Click(object sender, RoutedEventArgs e)
+        //{
+        //    winBasicDataBinding win = new winBasicDataBinding();
+
+        //    win.Show();
+        //}
+
+        //private void btnOtherBinding_Click(object sender, RoutedEventArgs e)
+        //{
+        //    winMoreDataBinding win = new winMoreDataBinding();
+
+        //    win.Show();
+        //}
+
+        //private void btnSimpleMVVM_Click(object sender, RoutedEventArgs e)
+        //{
+        //    winSimpleMVVM win = new winSimpleMVVM();
+
+        //    win.Show();
+        //}
+    }
+}
